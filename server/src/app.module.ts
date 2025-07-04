ab84c0e5126bd7f8ad42ffe46959d26751ba6aac
Assaf Koren <assafnoahkoren@gmail.com>
2025-07-05 02:46:07 +0300

    .

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrpcModule } from './trpc/trpc.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [TrpcModule],
+  imports: [TrpcModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
